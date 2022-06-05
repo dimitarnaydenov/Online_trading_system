d9bb2046a3a9eb81c50f6bedf45a79c11613efbc
Dimanaa <95311278+Dimanaa@users.noreply.github.com>
2022-06-05 21:11:54 +0300

    Update HomeController.java

--- src/main/java/nbu/onlinetradingsytem/controller/HomeController.java
+++ src/main/java/nbu/onlinetradingsytem/controller/HomeController.java
@@ -27,6 +27,37 @@ public class HomeController {
 
         return "redirect:/users";
     }
+    
+     @GetMapping("/register")
+    public String getRegisterPage(Model model) {
+        model.addAttribute("registerRequest", new User());
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String register(@ModelAttribute User user) {
+        System.out.println("register request: " + user);
+        User userToRegister = userService.registerUser(user.getFirstName(), user.getLastName(), user.getUsername(), user.getPassword());
+        return userToRegister == null ? "error_page" : "redirect:/login";
+    }
+
+    @GetMapping("/login")
+    public String getLoginPage(Model model) {
+        model.addAttribute("loginRequest");
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String login(@ModelAttribute User user, Model model) {
+        System.out.println("login request: " + user);
+        User authUser = userService.auth(user.getUsername(), user.getPassword());
+        if(authUser != null) {
+            model.addAttribute("userLogin", authUser.getUsername());
+            return "users";
+        } else {
+            return "error_page";
+        }
+    }
 
     @GetMapping("/users")
     public String showUsers(Model model) {
