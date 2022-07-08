7b6273eb0de720d1b8ca9b547f91ab4e8b3b4cf5
Dimitar Naydenov <mitkoik@gmail.com>
2022-07-08 20:43:08 +0300

    Update SecurityConfiguration.java

--- src/main/java/nbu/onlinetradingsytem/config/SecurityConfiguration.java
+++ src/main/java/nbu/onlinetradingsytem/config/SecurityConfiguration.java
@@ -4,6 +4,7 @@ import nbu.onlinetradingsytem.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -23,7 +24,24 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .authorizeRequests().anyRequest().permitAll()
+                .authorizeRequests()
+                .antMatchers("/cart").authenticated()
+                .antMatchers(HttpMethod.GET, "/statistics").hasAuthority("ADMIN")
+                .antMatchers(HttpMethod.GET, "/users").hasAuthority("ADMIN")
+                .antMatchers(HttpMethod.GET, "/editUser").hasAuthority("ADMIN")
+                .antMatchers(HttpMethod.POST, "/editUser").hasAuthority("ADMIN")
+                .antMatchers(HttpMethod.GET, "/store").hasAuthority("ADMIN")
+                .antMatchers(HttpMethod.POST, "/store").hasAuthority("ADMIN")
+                .antMatchers(HttpMethod.GET, "/deleteUser").hasAuthority("ADMIN")
+                .antMatchers(HttpMethod.GET, "/suppliers").hasAnyAuthority("ADMIN", "MODERATOR")
+                .antMatchers(HttpMethod.GET, "/editSupplier").hasAnyAuthority("ADMIN", "MODERATOR")
+                .antMatchers(HttpMethod.GET, "/editProduct").hasAnyAuthority("ADMIN", "MODERATOR")
+                .antMatchers(HttpMethod.POST, "/editSupplier").hasAnyAuthority("ADMIN", "MODERATOR")
+                .antMatchers(HttpMethod.POST, "/editProduct").hasAnyAuthority("ADMIN", "MODERATOR")
+                .antMatchers(HttpMethod.GET, "/addProduct").hasAnyAuthority("ADMIN", "MODERATOR")
+                .antMatchers(HttpMethod.POST, "/addProduct").hasAnyAuthority("ADMIN", "MODERATOR")
+                .antMatchers(HttpMethod.GET, "/deleteSupplier").hasAnyAuthority("ADMIN", "MODERATOR")
+                .antMatchers(HttpMethod.GET, "/deleteProduct").hasAnyAuthority("ADMIN", "MODERATOR")
 //                .antMatchers("/register",
 //                        "/login",
 //                        "/users",
@@ -39,7 +57,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .loginPage("/login")
                 .loginProcessingUrl("/login")
                 .defaultSuccessUrl("/", true)
-                .failureUrl("/login")
+                .failureUrl("/login?id=error")
                 .permitAll()
                 .and()
                 .logout()
